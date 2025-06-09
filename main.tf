3c118d1b875b4fd98a6cf2573f6c67ba781e1698
priyaraghunath <priyankabraghunath98@gmail.com>
2025-06-09 17:28:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,43 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"  
+  region = "us-west-2" 
 }
 
-# IAM Role for EC2
+# S3 Bucket
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-ecomweb-bucket-name-20250609" 
+  tags = {
+    Name        = "ExampleS3Bucket"
+    Environment = "Production"
+  }
+}
+
+# S3 Bucket ACL (recommended way)
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.example_bucket.id
+  acl    = "private"
+}
+
+# S3 Bucket Policy (public read access to all objects)
+resource "aws_s3_bucket_policy" "example_bucket_policy" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.example_bucket.bucket}/*"
+      }
+    ]
+  })
+}
+
+# IAM Role for EC2 to access S3 (optional, but best practice)
 resource "aws_iam_role" "ec2_role" {
   name = "ec2_s3_access_role"
   assume_role_policy = jsonencode({
@@ -27,13 +60,12 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-# IAM Policy for S3 access
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions   = ["s3:*"]
     resources = [
-      "arn:aws:s3:::my-ecomwebsite-bucket-name-20250609",
-      "arn:aws:s3:::my-ecomwebsite-bucket-name-20250609/*"
+      "arn:aws:s3:::${aws_s3_bucket.example_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.example_bucket.bucket}/*"
     ]
   }
 }
@@ -48,7 +80,6 @@ resource "aws_iam_role_policy_attachment" "ec2_attach_s3" {
   policy_arn = aws_iam_policy.s3_access.arn
 }
 
-# Instance Profile for EC2
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2_instance_profile"
   role = aws_iam_role.ec2_role.name
@@ -56,20 +87,11 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 # EC2 Instance
 resource "aws_instance" "example_server" {
-  ami                  = "ami-0418306302097dbff"  # Replace with valid AMI ID for your region
+  ami                  = "ami-0418306302097dbff" # Replace with a valid AMI for your region
   instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
-    Name = "E-comInstance"
-  }
-}
-
-# S3 Bucket
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-ecomwebsite-bucket-name-20250609"  
-  acl    = "private"
-
-  tags = {
-    Name        = "ExampleS3Bucket"
+    Name = "ExampleInstance"
   }
 }
